ea5dcecab690735c736a7cfc0da1b46dedb45fb7
HAMZA NASIR <hamza@volstream-systems.com>
2024-05-22 12:37:35 +0500

    Add project files.

---
+++ WebApp_UnderTheHood/Pages/Account/Login.cshtml.cs
@@ -0,0 +1,64 @@
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.ComponentModel.DataAnnotations;
+using System.Net;
+using System.Security.Claims;
+
+namespace WebApp_UnderTheHood.Pages.Account
+{
+    public class LoginModel : PageModel
+    {
+        [BindProperty]
+        public Credential Credential { get; set; } = new Credential();
+
+        public void OnGet()
+        {
+        }
+
+		public async Task<IActionResult> OnPost()
+		{
+            if (!ModelState.IsValid) return Page();
+
+            //Verify the credentials
+            if(Credential.UserName == "admin" && Credential.Password == "password")
+            {
+				//Creating the security context.
+				List<Claim> claims = new List<Claim>
+                {
+                    new Claim(ClaimTypes.Name, "admin"),
+                    new Claim(ClaimTypes.Email, "admin@email.com"),
+                    new Claim("Department", "HR"),
+                    new Claim("Manager", "true"),
+                    new Claim("Admin", "true"),
+                    new Claim("EmploymentDate", "2024-02-01")
+                };
+
+				ClaimsIdentity identity = new ClaimsIdentity(claims, "MyCookieAuth");   //See its function signature.
+                ClaimsPrincipal principal = new ClaimsPrincipal(identity);
+
+                await HttpContext.SignInAsync("MyCookieAuth", principal); //SignInAsync is an extension method
+
+                return RedirectToPage("/Index");
+            }else
+            {
+				// Add a model error for invalid credentials
+				ModelState.AddModelError(string.Empty, "Invalid username or password");
+			}
+
+            return Page();
+		}
+	}
+
+    public class Credential
+    {
+        [Required]
+        [Display(Name = "User Name")]
+        public string UserName { get; set; } = string.Empty;
+ 
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; } = string.Empty;
+
+    }
+}
---
+++ WebApp_UnderTheHood/Program.cs
@@ -0,0 +1,45 @@
+using Microsoft.AspNetCore.Authorization;
+using WebApp_UnderTheHood.Authorization;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+
+builder.Services.AddAuthentication("MyCookieAuth").AddCookie("MyCookieAuth", options =>
+{
+    options.Cookie.Name = "MyCookieAuth";
+    //options.LoginPath = "/AccountFolder/LoginPage";  //Default path ==> /Account/Login
+    //options.AccessDeniedPath = "/AccountFolder/AccessDeniedPage";
+});
+
+builder.Services.AddAuthorization(options =>
+{
+    options.AddPolicy("AdminOnly", policy => policy.RequireClaim("Admin"));
+    options.AddPolicy("MustBelongToHRDepartment", policy => policy.RequireClaim("Department","HR"));
+    options.AddPolicy("HRManagerOnly", policy => policy.RequireClaim("Department", "HR").RequireClaim("Manager").Requirements.Add(new HRManagerProbationRequirement(3)));
+});
+
+builder.Services.AddSingleton<IAuthorizationHandler, HRManagerProbationRequirementHandler>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapRazorPages();
+
+app.Run();
